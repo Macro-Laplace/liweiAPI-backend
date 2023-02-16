60a7d5d3ee3a41c578896fdb0a4c6ffb400540d0
Macro-Laplace <810282264@qq.com>
2023-02-16 16:21:36 +0800

    修复新增查詢手机号属地的接口乱码bug

--- liweiAPI-interface/src/main/java/com/liwei/liweiapiinterface/controller/NameController.java
+++ liweiAPI-interface/src/main/java/com/liwei/liweiapiinterface/controller/NameController.java
@@ -1,10 +1,14 @@
 package com.liwei.liweiapiinterface.controller;
 
 
+import cn.hutool.core.swing.ScreenUtil;
+import cn.hutool.core.text.UnicodeUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 
+import cn.hutool.http.HttpUtil;
 import com.liwei.liweiapiclientsdk.model.User;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,10 +34,8 @@ public class NameController {
        //网关中已经进行校验和记录调用次数，这里就直接调用
 //        String result = "POST 用户名字是" + user.getUsername();
 //        return result;
-        HttpResponse httpResponse = HttpRequest.get("https://api.vvhan.com/api/phone?tel=" + user.getPhoneNumber())
-                .execute();
-        System.out.println(httpResponse.getStatus());
-        String result = httpResponse.body();
+        String result = HttpUtil.get("https://cx.shouji.360.cn/phonearea.php?number="+user.getPhoneNumber());
+        result=UnicodeUtil.toString(result);
         System.out.println(result);
         return result;
     }
--- liweiAPI-interface/src/test/java/com/liwei/liweiapiinterface/LiweiApiInterfaceApplicationTests.java
+++ liweiAPI-interface/src/test/java/com/liwei/liweiapiinterface/LiweiApiInterfaceApplicationTests.java
@@ -1,11 +1,20 @@
 package com.liwei.liweiapiinterface;
 
+import cn.hutool.core.text.UnicodeUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import com.liwei.liweiapiclientsdk.client.LiApiClient;
 import com.liwei.liweiapiclientsdk.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class LiweiApiInterfaceApplicationTests {
@@ -15,12 +24,10 @@ class LiweiApiInterfaceApplicationTests {
 
     @Test
     void contextLoads() {
-        String result = liApiClient.getNameByGet("liwei");
-        User user = new User();
-        user.setUsername("liwei");
-        String usernameByPost = liApiClient.getUsernameByPost(user);
+        //String result = HttpUtil.get("https://api.vvhan.com/api/phone?tel=13145211010");
+        String result = HttpUtil.get("https://cx.shouji.360.cn/phonearea.php?number=18396279786");
+        result=UnicodeUtil.toString(result);
         System.out.println(result);
-        System.out.println(usernameByPost);
     }
 
 }
