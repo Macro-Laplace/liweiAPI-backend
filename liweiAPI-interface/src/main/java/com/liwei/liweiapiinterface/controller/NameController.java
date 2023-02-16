fe5597d8bbd9691157ed8df0727e498b6d887a96
Macro-Laplace <810282264@qq.com>
2023-02-16 10:46:52 +0800

    新增查詢手机号属地的接口

--- liweiAPI-interface/src/main/java/com/liwei/liweiapiinterface/controller/NameController.java
+++ liweiAPI-interface/src/main/java/com/liwei/liweiapiinterface/controller/NameController.java
@@ -1,8 +1,10 @@
 package com.liwei.liweiapiinterface.controller;
 
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+
 import com.liwei.liweiapiclientsdk.model.User;
-import com.liwei.liweiapiclientsdk.utils.SignUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +28,13 @@ public class NameController {
     public String getUsernameByPost(@RequestBody User user, HttpServletRequest request) {
 
        //网关中已经进行校验和记录调用次数，这里就直接调用
-        String result = "POST 用户名字是" + user.getUsername();
+//        String result = "POST 用户名字是" + user.getUsername();
+//        return result;
+        HttpResponse httpResponse = HttpRequest.get("https://api.vvhan.com/api/phone?tel=" + user.getPhoneNumber())
+                .execute();
+        System.out.println(httpResponse.getStatus());
+        String result = httpResponse.body();
+        System.out.println(result);
         return result;
     }
 }
